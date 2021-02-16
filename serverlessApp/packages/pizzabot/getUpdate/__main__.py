91182bd1fcd6dda368650b244891c79f9a2efb51
Filippo Valle <filippo.valle@edu.unito.it>
2021-02-16 10:46:41 +0100

    return

--- serverlessApp/packages/pizzabot/getUpdate/__main__.py
+++ serverlessApp/packages/pizzabot/getUpdate/__main__.py
@@ -3,5 +3,7 @@ from pizza_bot import parse_message
 
 def main(params):
 	parse_message(params)
-	return {"message": json.dumps(params),
+	return {"body":
+		{"message": json.dumps(params),
 		"status": 400}
+		}
